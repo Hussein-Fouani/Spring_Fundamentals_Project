6ef29b3b8f6ac804b86568c3be3652a1832058b7
Hussein <social.101.acc@gmail.com>
2023-09-25 17:46:02 +0300

    added the mapping to post request to save the beer

--- src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
+++ src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
@@ -4,11 +4,9 @@ import com.hf.spring_fundamentals_project.model.Beer;
 import com.hf.spring_fundamentals_project.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,7 +17,12 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+    public ResponseEntity handePost(Beer beer){
+        Beer beer1 = beerService.savenewBeer(beer);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 
+    @PostMapping
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
