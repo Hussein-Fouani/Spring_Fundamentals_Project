263909fa71af9099efbb3658c27014ae79ed5a59
Hussein <social.101.acc@gmail.com>
2023-09-25 14:50:47 +0300

    Added a Map for each beer along its UUID in the BeerServiceImpl as a harcodded data

--- src/main/java/com/hf/spring_fundamentals_project/service/BeerServiceImpl.java
+++ src/main/java/com/hf/spring_fundamentals_project/service/BeerServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -14,16 +15,64 @@ import java.util.UUID;
 public class BeerServiceImpl implements BeerService {
     BeerService beerService;
 
+    public Map<UUID, Beer> beerMap;
+
     @Override
     public Beer getBeerById(UUID id) {
         return Beer.builder().id(id)
-                        .version(1)
-                                .beerName("Galaxy Cat")
-                                        .beerStyle(BeerStyle.Pale_ALe)
-                                                .upc("12345")
-                                                        .price(new BigDecimal("13.06"))
-                                                                .quantityonHand("122")
-                                                                        .createdDate(LocalDateTime.now())
-                                                                                .updatedDate(LocalDateTime.now()).build();
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12345")
+                .price(new BigDecimal("13.06"))
+                .quantityonHand(122)
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now()).build();
+    }
+
+    public BeerServiceImpl(BeerService beerService) {
+        this.beerService = beerService;
+
+
+        Beer beer1 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityonHand(122)
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .build();
+
+        Beer beer2 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Crank")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356222")
+                .price(new BigDecimal("11.99"))
+                .quantityonHand(392)
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Sunshine City")
+                .beerStyle(BeerStyle.IPA)
+                .upc("12356")
+                .price(new BigDecimal("13.99"))
+                .quantityonHand(144)
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .build();
+
+        beerMap.put (beer1.getId(), beer1);
+        beerMap.put(beer2.getId(), beer2);
+        beerMap.put(beer3.getId(), beer3);
     }
 }
+
