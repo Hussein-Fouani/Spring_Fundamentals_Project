1d6e07377c8fe84b0892a5b63a3bc9fdc0438ec0
Hussein <social.101.acc@gmail.com>
2023-09-25 12:07:30 +0300

    implemented hardcodded getbeerbyId method using builder class

--- src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
+++ src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
@@ -1,11 +1,20 @@
 package com.hf.spring_fundamentals_project.controller;
 
+import com.hf.spring_fundamentals_project.model.Beer;
 import com.hf.spring_fundamentals_project.service.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
 @Controller
+@Slf4j
 @AllArgsConstructor
 public class BeerController {
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+        return beerService.getBeerById(id);
+    }
 }
