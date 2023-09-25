8383edd1cadfa8c08cedb350def321adf101850d
Hussein <social.101.acc@gmail.com>
2023-09-25 16:01:27 +0300

    added the path for the api by using RequestMapping annotation

--- src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
+++ src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
@@ -5,16 +5,27 @@ import com.hf.spring_fundamentals_project.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @Slf4j
 @AllArgsConstructor
 public class BeerController {
     private final BeerService beerService;
 
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+
+    }
+
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Bear by ID -in services. id" + id.toString());
         return beerService.getBeerById(id);
     }
 }
