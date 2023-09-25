479cbb8bc22c3d2e1eddeaf6db82701030a61f55
Hussein <social.101.acc@gmail.com>
2023-09-25 16:03:20 +0300

    initialising the listbeers in the serviceimpl class to avoid nullpointerexception and removed the cicular dependency between it and the controller

--- src/main/java/com/hf/spring_fundamentals_project/service/BeerService.java
+++ src/main/java/com/hf/spring_fundamentals_project/service/BeerService.java
@@ -2,8 +2,11 @@ package com.hf.spring_fundamentals_project.service;
 
 import com.hf.spring_fundamentals_project.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
--- src/main/java/com/hf/spring_fundamentals_project/service/BeerServiceImpl.java
+++ src/main/java/com/hf/spring_fundamentals_project/service/BeerServiceImpl.java
@@ -7,33 +7,27 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @Slf4j
 public class BeerServiceImpl implements BeerService {
     BeerService beerService;
 
+    @Override
+    public List<Beer> listBeers(){
+        return new ArrayList<>(beerMap.values());
+    }
+
     public Map<UUID, Beer> beerMap;
 
     @Override
     public Beer getBeerById(UUID id) {
-        return Beer.builder().id(id)
-                .version(1)
-                .beerName("Galaxy Cat")
-                .beerStyle(BeerStyle.PALE_ALE)
-                .upc("12345")
-                .price(new BigDecimal("13.06"))
-                .quantityonHand(122)
-                .createdDate(LocalDateTime.now())
-                .updatedDate(LocalDateTime.now()).build();
+        return beerMap.get(id);
     }
 
-    public BeerServiceImpl(BeerService beerService) {
-        this.beerService = beerService;
-
-
+    public BeerServiceImpl() {
+        beerMap = new HashMap<>();
         Beer beer1 = Beer.builder()
                 .id(UUID.randomUUID())
                 .version(1)
