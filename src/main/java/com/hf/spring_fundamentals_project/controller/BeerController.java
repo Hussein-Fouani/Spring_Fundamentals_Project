8d381cacfc69c9b55a229616de99d030a4c813de
Hussein <social.101.acc@gmail.com>
2023-09-25 16:12:30 +0300

    allowing Request-method to get to be applied on listing-beers api and added the method for single beer request

--- src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
+++ src/main/java/com/hf/spring_fundamentals_project/controller/BeerController.java
@@ -5,7 +5,9 @@ import com.hf.spring_fundamentals_project.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,18 +16,19 @@ import java.util.UUID;
 @RestController
 @Slf4j
 @AllArgsConstructor
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
 
     }
+    @RequestMapping(value = "{beerId}",method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
 
-    public Beer getBeerById(UUID id) {
-
-        log.debug("Get Bear by ID -in services. id" + id.toString());
-        return beerService.getBeerById(id);
+        log.debug("Get Bear by ID -in services. id" + beerId.toString());
+        return beerService.getBeerById(beerId);
     }
 }
