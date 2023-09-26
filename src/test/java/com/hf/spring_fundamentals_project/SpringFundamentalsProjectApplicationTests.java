56b0ca7e630b18f321772b59cba05693c026d85a
Hussein <social.101.acc@gmail.com>
2023-09-26 13:44:13 +0300

    started learning the testing and unit testing concepts,applied the webmvctest for controllers

--- src/test/java/com/hf/spring_fundamentals_project/SpringFundamentalsProjectApplicationTests.java
+++ src/test/java/com/hf/spring_fundamentals_project/SpringFundamentalsProjectApplicationTests.java
@@ -1,13 +1,25 @@
 package com.hf.spring_fundamentals_project;
 
+import com.hf.spring_fundamentals_project.controller.BeerController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.awt.*;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
 class SpringFundamentalsProjectApplicationTests {
 
+    MockMvc mockito;
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mockito.perform(get("api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 
 }
