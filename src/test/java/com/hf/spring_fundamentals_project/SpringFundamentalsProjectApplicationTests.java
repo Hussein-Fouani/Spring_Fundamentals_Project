ccfaeb2b3aaa391c13d97da266b33eeba331eb7e
Hussein <social.101.acc@gmail.com>
2023-09-26 14:58:36 +0300

    mockmvc test listbeers

--- src/test/java/com/hf/spring_fundamentals_project/SpringFundamentalsProjectApplicationTests.java
+++ src/test/java/com/hf/spring_fundamentals_project/SpringFundamentalsProjectApplicationTests.java
@@ -4,6 +4,7 @@ import com.hf.spring_fundamentals_project.controller.BeerController;
 import com.hf.spring_fundamentals_project.model.Beer;
 import com.hf.spring_fundamentals_project.service.BeerService;
 import com.hf.spring_fundamentals_project.service.BeerServiceImpl;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,11 +15,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.awt.*;
 import java.util.UUID;
 
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BeerController.class)
 class SpringFundamentalsProjectApplicationTests {
@@ -29,12 +30,20 @@ class SpringFundamentalsProjectApplicationTests {
     BeerService beerService;
     BeerServiceImpl beerServiceimpl = new BeerServiceImpl();
 
+    @Test
+    void testlistBeers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceimpl.listBeers());
+
+        mockito.perform(get("api/v1/beer").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()",is(3)));
+    }
 
     @Test
     void contextLoads() throws Exception {
-        mockito.perform(get("api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
         Beer testcase = beerServiceimpl.listBeers().get(0);
-        given(beerService.getBeerById(any(UUID.class))).willReturn(testcase);
+        mockito.perform(get("api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect( jsonPath("$.id",is(testcase.getId().toString())));
+        given(beerService.getBeerById(testcase.getId())).willReturn(testcase);
     }
 
 }
