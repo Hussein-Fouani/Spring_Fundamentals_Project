aff581971bc2a73161e5fe8c5263887611015969
Hussein <social.101.acc@gmail.com>
2023-09-29 15:31:37 +0300

    test for testById BeerNotFound implementation ,which will throw not found exception

--- src/test/java/com/hf/spring_fundamentals_project/controller/BeerControllerIT.java
+++ src/test/java/com/hf/spring_fundamentals_project/controller/BeerControllerIT.java
@@ -1,6 +1,7 @@
 package com.hf.spring_fundamentals_project.controller;
 
 import com.hf.spring_fundamentals_project.entities.BeerDTO;
+import com.hf.spring_fundamentals_project.model.Beer;
 import com.hf.spring_fundamentals_project.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,6 +29,19 @@ class BeerControllerIT {
        List<BeerDTO> beerDTOList =beerController.listBeers();
         assertThat(beerDTOList.size()).isEqualTo(3);
 
+    }
+
+    @Test
+    void testByIdBeerNotFound() {
+        assertThrows(NotFoundException.class,()-> beerController.getBeerById(UUID.randomUUID()));
+
+    }
+
+    @Test
+    void testBeerById() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO beerDTO = beerController.getBeerById(beer.getId());
+        assertThat(beerDTO).isNotNull();
     }
 
     @Transactional
