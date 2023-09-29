f964a7bf3205d1c74605fd17fa49c78bb147e12c
Hussein <social.101.acc@gmail.com>
2023-09-29 12:24:00 +0300

    creating the BeerRepository

---
+++ src/main/java/com/hf/spring_fundamentals_project/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.hf.spring_fundamentals_project.repositories;
+
+import com.hf.spring_fundamentals_project.model.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
